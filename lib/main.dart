134a829efe0ea1facc43a9ea42c7ff20e32ef8d0
Richard Garcia <richardgcia04@gmail.com>
2020-10-20 21:25:58 -0500

    nueva carpeta providers y archivo dentro de ella

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:push_notifications/src/pages/home_page.dart';
 import 'package:push_notifications/src/pages/mensaje_page.dart';
+import 'package:push_notifications/src/providers/push_notifications_provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    final pushProvider = new PushNotificationsProvider();
+    pushProvider.initNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
